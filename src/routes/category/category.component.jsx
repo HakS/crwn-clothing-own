11e0bccc4e250e307d4765be793d376c2645a39e
Juan Barba <juan.barba.o@gmail.com>
2024-06-14 23:14:30 +0200

    Created routes for categories list and category page, made context to handle category data from Firestore, made a utility function to add data to Firestore

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,25 @@
+import { useParams } from 'react-router-dom'
+import { useContext } from 'react'
+import { CategoriesContext } from '../../contexts/categories.context'
+import ProductCard from '../../components/product-card/product-card.component';
+
+import './category.styles.scss'
+
+const Category = () => {
+  const { category } = useParams()
+  const { categoriesMap } = useContext(CategoriesContext)
+  console.log("RERENDER CATEGORY?", category, categoriesMap)
+
+  return (
+    <>
+      <h2 className='category-title'>{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {(categoriesMap[category])?.map((product) =>
+          <ProductCard key={product.id} product={product} />
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Category
