71779e3b88bacce848bb0372f51470b637ab3b99
Juan Barba <juan.barba.o@gmail.com>
2024-06-21 20:11:07 +0200

    Removed unused vars

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,4 @@
 import { CartIconContainer, StyledShoppingIcon, ItemCount } from './cart-icon.styles'
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import { useContext } from 'react'
 import { CartContext } from '../../contexts/cart.context';
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 
