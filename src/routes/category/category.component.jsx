d0940dcda282b08a16ac31f6e7390ddec7609f7b
Juan Barba <juan.barba.o@gmail.com>
2024-06-22 23:00:41 +0200

    Installed redux thunk and redux saga, changed categories implementation to use redux thunk (made async action for loading categories), created spinner component for loading data, made sync reducers to modify the loading state of categories

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,22 +2,25 @@ import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
+import Spinner from '../../components/spinner/spinner.component';
 
 import './category.styles.scss'
 
 const Category = () => {
   const { category } = useParams()
+  const isLoading = useSelector(selectCategoriesIsLoading)
   const categoriesMap = useSelector(selectCategoriesMap)
 
   return (
     <>
       <h2 className='category-title'>{category.toUpperCase()}</h2>
-      <div className="category-container">
-        {(categoriesMap[category])?.map((product) =>
-          <ProductCard key={product.id} product={product} />
-        )}
-      </div>
+      { isLoading ? <Spinner /> :
+        <div className="category-container">
+          {(categoriesMap[category])?.map((product) =>
+            <ProductCard key={product.id} product={product} />
+          )}
+        </div> }
     </>
   )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,25 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-
-import { rootReducer } from "./root-reducer";
+import { thunk } from "redux-thunk";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+
+import { rootReducer } from "./root-reducer";
 import { syncLogger } from './middleware/logger';
 
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user']
+  whitelist: ['cart']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const middleWares = [
   process.env.NODE_ENV !== 'production'
-    && syncLogger
-  // && logger,
+    // && syncLogger,
+  && logger,
+  thunk,
 ].filter(Boolean)
 
 const composerEnhancer = (
