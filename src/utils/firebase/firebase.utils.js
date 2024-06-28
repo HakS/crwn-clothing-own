9e02d116d81b1ab451eb6ccfde5e9e13f5c758c6
Juan Barba <juan.barba.o@gmail.com>
2024-06-28 11:21:08 +0200

    Installed and set up Stripe, moved keys to env vars (need deployment setup)

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,12 +3,12 @@ import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, creat
 import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhO55di9NrGUHFI0uqw3PriQeuAR0EtFI",
-  authDomain: "crwn-clothing-db-affff.firebaseapp.com",
-  projectId: "crwn-clothing-db-affff",
-  storageBucket: "crwn-clothing-db-affff.appspot.com",
-  messagingSenderId: "977558983036",
-  appId: "1:977558983036:web:4ee95366b19fce88ddd4f9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
