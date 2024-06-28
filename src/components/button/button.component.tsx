11a22a3bfad24844188596c7fbb831c4daa578bb
Juan Barba <juan.barba.o@gmail.com>
2024-06-28 13:43:13 +0200

    Installed Typescript and converted first component for testing

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,9 +1,15 @@
 import {BaseButton, ButtonSpinner, GoogleSignInButton, InvertedButton} from './button.styles'
 
-export const BUTTON_TYPE_CLASSES = {
-  base: 'base',
-  google: 'google-sign-in',
-  inverted: 'inverted'
+export enum BUTTON_TYPE_CLASSES {
+  base = 'base',
+  google = 'google-sign-in',
+  inverted = 'inverted'
+}
+
+type Props = {
+  buttonType: BUTTON_TYPE_CLASSES,
+  isLoading: boolean,
+  [x:string]: any;
 }
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => ({
@@ -12,7 +18,7 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => ({
   [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
 }[buttonType])
 
-const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }: Props) => {
   const CustomButton = getButton(buttonType)
   return <CustomButton disabled={isLoading} {...otherProps} >
     {isLoading ? <ButtonSpinner /> : children}
